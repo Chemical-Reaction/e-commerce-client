6d8590cf40a2147decaf335549baa72fa8dab18f
areeshz <azindani786@gmail.com>
2020-07-15 16:04:23 -0400

    Add basic Product component, route, and link
    Create basic Product component that displays name, description, price, and some fake buttons
    Add Products link in Header that links to the /products route
    Add /products route in App.js that renders a map of all the products as individual Product components
    All products currently hard-coded in the front end

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
+import Product from '../Product'
 
 class App extends Component {
   constructor () {
@@ -18,6 +19,20 @@ class App extends Component {
       msgAlerts: []
     }
   }
+
+  productList = [{
+    name: 'apple',
+    description: 'a good fruit',
+    price: 5
+  }, {
+    name: 'banana',
+    description: 'a yellow fruit',
+    price: 3
+  }, {
+    name: 'orange',
+    description: 'citrus fruit',
+    price: 75
+  }]
 
   setUser = user => this.setState({ user })
 
@@ -47,6 +62,11 @@ class App extends Component {
           )} />
           <Route path='/sign-in' render={() => (
             <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
+          )} />
+          <Route path="/products" render={() => (
+            this.productList.map(product => (
+              <Product key={product.name} name={product.name} description={product.description} price={product.price} />
+            ))
           )} />
           <AuthenticatedRoute user={user} path='/sign-out' render={() => (
             <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
