8862213e68322b25e81fba51a6f4b2a791fe29a5
areeshz <azindani786@gmail.com>
2020-07-16 12:02:27 -0400

    Add ProductPage component, modify header links, and modify App routes
    Add ProductPage component that renders all products from the api as Product components
    App.js add ProductPage as render on the /products route, and switch it to an AuthenticatedRoute
    Header.js move "products" button from unauthenticatedOptions to authenticatedOptions
    Product.js set up basic styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,8 @@ import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
-import Product from '../Product'
+import ProductPage from '../ProductPage'
+// import Product from '../Product'
 
 class App extends Component {
   constructor () {
@@ -19,20 +20,6 @@ class App extends Component {
       msgAlerts: []
     }
   }
-
-  productList = [{
-    name: 'apple',
-    description: 'a good fruit',
-    price: 5
-  }, {
-    name: 'banana',
-    description: 'a yellow fruit',
-    price: 3
-  }, {
-    name: 'orange',
-    description: 'citrus fruit',
-    price: 75
-  }]
 
   setUser = user => this.setState({ user })
 
@@ -63,10 +50,8 @@ class App extends Component {
           <Route path='/sign-in' render={() => (
             <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
           )} />
-          <Route path="/products" render={() => (
-            this.productList.map(product => (
-              <Product key={product.name} name={product.name} description={product.description} price={product.price} />
-            ))
+          <AuthenticatedRoute user={user} path="/products" render={() => (
+            <ProductPage token={user.token} />
           )} />
           <AuthenticatedRoute user={user} path='/sign-out' render={() => (
             <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Product extends Component {
-  render () {
-    return (
-      <div style={{ border: '3px solid black', margin: '10px', padding: '5px', width: '300px' }}>
-        <h2>Name: {this.props.name}</h2>
-        <p>Description: {this.props.description}</p>
-        <p>Price: ${this.props.price}</p>
-        <button>Please Sign in to add to cart</button>
-        <br />
-        <button>Add to Cart</button>
-      </div>
-    )
+const Product = (props) => {
+  const handleAddtoCart = (event) => {
+    event.preventDefault()
+    console.log('you clicked the button')
   }
+
+  const productStyles = {
+    border: '3px solid black',
+    margin: '10px',
+    padding: '5px',
+    width: '300px'
+  }
+
+  const addButtonStyles = {
+    color: 'black'
+  }
+
+  return (
+    <div style={productStyles}>
+      <h2>Name: {props.name}</h2>
+      <p>Description: {props.description}</p>
+      <p>Price: ${props.price}</p>
+      <br />
+      <button style={addButtonStyles} onClick={handleAddtoCart}>Add to Cart</button>
+    </div>
+  )
 }
 
 export default Product
