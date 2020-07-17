9adbd8143ef6029e766a4829fe19d2a39e31c787
areeshz <azindani786@gmail.com>
2020-07-17 12:39:12 -0400

    Added apiUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://lit-reef-17341.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
