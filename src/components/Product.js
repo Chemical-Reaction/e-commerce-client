179e687a5821af31cd7840594aae8a680407a928
Dajana8127 <dajana.stankovic94@gmail.com>
2020-07-17 11:54:23 -0400

    Adding add to cart button
    Add to cart button has some functionality right now, but the duplicate
    item keys is preventing the cart and removing from cart from functioning
     properly

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import axios from 'axios'
+import apiUrl from './../apiConfig'
 
 const Product = (props) => {
   const handleAddtoCart = (event) => {
-    event.preventDefault()
     console.log('you clicked the button')
+    console.log('product id', props.productId)
+    console.log('cart is', props.cart)
+    props.cart.products.push(props.productId)
+
+    axios({
+      method: 'PATCH',
+      url: apiUrl + `/orders/${props.cart._id}`,
+      headers: {
+        'Authorization': `Bearer ${props.token}`
+      },
+      data: {
+        order: props.cart
+      }
+    })
+      .then((response) => {
+        console.log('this is my patch response', response)
+      })
+      .catch(console.error)
   }
 
   const productStyles = {
