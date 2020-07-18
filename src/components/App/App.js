acec94d7a1d739679e8d727f242e5dde0d61496e
areeshz <azindani786@gmail.com>
2020-07-18 14:48:30 -0400

    Basic Stripe integration on checkout page
    Stripe integration on checkout page, works with Stripe testing api, and currently only logs 'payment successful' to the console upon payment success
    App.js added stripe imports and <Elements> wrapper

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,6 +15,11 @@ import Cart from '../Cart'
 
 import PastOrderPage from '../PastOrderPage'
 
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+const promise = loadStripe('pk_test_51H66m5Bl2OnK3EGGkcTtz68xN5pTzIIQrToRqtmD27h6IAaeHwuravCHArscG3qn2PKYtszTB7fGLspUtvNQ9qJm00q5Y8REjS')
+
 // import Product from '../Product'
 
 class App extends Component {
@@ -40,41 +45,43 @@ class App extends Component {
 
     return (
       <Fragment>
-        <Header user={user} />
-        {msgAlerts.map((msgAlert, index) => (
-          <AutoDismissAlert
-            key={index}
-            heading={msgAlert.heading}
-            variant={msgAlert.variant}
-            message={msgAlert.message}
-          />
-        ))}
-        <main className="container">
-          <Route path='/sign-up' render={() => (
-            <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path="/products" render={() => (
-            <ProductPage token={user.token} />
-          )} />
-          <AuthenticatedRoute user={user} path="/past-orders" render={() => (
-            <PastOrderPage token={user.token} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword msgAlert={this.msgAlert} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/cart' render={() => (
-            <Cart token={user.token} />
-          )} />
-          <AuthenticatedRoute user={user} path='/checkout' render={(componentProps) => (
-            <CheckoutPage redirectState={componentProps} user={user}/>
-          )} />
-        </main>
+        <Elements stripe={promise} >
+          <Header user={user} />
+          {msgAlerts.map((msgAlert, index) => (
+            <AutoDismissAlert
+              key={index}
+              heading={msgAlert.heading}
+              variant={msgAlert.variant}
+              message={msgAlert.message}
+            />
+          ))}
+          <main className="container">
+            <Route path='/sign-up' render={() => (
+              <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
+            )} />
+            <Route path='/sign-in' render={() => (
+              <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
+            )} />
+            <AuthenticatedRoute user={user} path="/products" render={() => (
+              <ProductPage token={user.token} />
+            )} />
+            <AuthenticatedRoute user={user} path="/past-orders" render={() => (
+              <PastOrderPage token={user.token} />
+            )} />
+            <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+              <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} path='/change-password' render={() => (
+              <ChangePassword msgAlert={this.msgAlert} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} path='/cart' render={() => (
+              <Cart token={user.token} />
+            )} />
+            <AuthenticatedRoute user={user} path='/checkout' render={(componentProps) => (
+              <CheckoutPage redirectState={componentProps} user={user}/>
+            )} />
+          </main>
+        </Elements>
       </Fragment>
     )
   }
--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,13 +1,94 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+import apiUrl from './../apiConfig'
+
+import {
+  CardElement,
+  useStripe,
+  useElements
+} from '@stripe/react-stripe-js'
 
 const CheckoutPage = (props) => {
+  const [succeeded, setSucceeded] = useState(false)
+  const [error, setError] = useState(null)
+  const [processing, setProcessing] = useState('')
+  const [disabled, setDisabled] = useState(true)
+  const [clientSecret, setClientSecret] = useState('')
+  const stripe = useStripe()
+  const elements = useElements()
+
   console.log('props on checkoutpage', props)
   const { subtotal, items } = props.redirectState.location.state
   const tax = Math.round((subtotal * 0.075) * 100) / 100
   const total = subtotal + tax
 
   const [backToCart, setBackToCart] = useState(false)
+
+  useEffect(() => {
+    console.log('useEffect is being run')
+    axios({
+      method: 'POST',
+      url: apiUrl + '/create-payment-intent',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        amount: total
+      }
+    })
+      .then(data => {
+        console.log('this is response data', data)
+        setClientSecret(data.data.clientSecret)
+      })
+  }, [])
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: '#32325d',
+        fontFamily: 'Roboto, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#32325d'
+        }
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a'
+      }
+    }
+  }
+
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty)
+    setError(event.error ? event.error.message : '')
+  }
+
+  const handleSubmit = async ev => {
+    ev.preventDefault()
+    setProcessing(true)
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: ev.target.name.value
+        }
+      }
+    })
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false)
+    } else {
+      setError(null)
+      setProcessing(false)
+      setSucceeded(true)
+      console.log('payment succeeded')
+    }
+  }
 
   const toCart = (event) => {
     setBackToCart(true)
@@ -38,6 +119,38 @@ const CheckoutPage = (props) => {
       <p>Sales Tax (7.5%): ${tax.toFixed(2)}</p>
       <h3>Order Total: ${total.toFixed(2)}</h3>
       <p>Your email is: {props.user.email}</p>
+      <br />
+      <form id="payment-form" onSubmit={handleSubmit}>
+        <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
+        <button
+          disabled={processing || disabled || succeeded}
+          id="submit"
+        >
+          <span id="button-text">
+            {processing ? (
+              <div className="spinner" id="spinner"></div>
+            ) : (
+              'Pay'
+            )}
+          </span>
+        </button>
+        {/* Show any error that happens when processing the payment */}
+        {error && (
+          <div className="card-error" role="alert">
+            {error}
+          </div>
+        )}
+        {/* Show a success message upon completion */}
+        <p className={succeeded ? 'result-message' : 'result-message hidden'}>
+          Payment succeeded, see the result in your
+          <a
+            href={'https://dashboard.stripe.com/test/payments'}
+          >
+            {' '}
+            Stripe dashboard.
+          </a> Refresh the page to pay again.
+        </p>
+      </form>
     </div>
   )
 }
